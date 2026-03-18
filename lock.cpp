60c176d51f9db605cecaae2f4b09c30a7fc3303e
SuryalalSingha <singhasuryalal@gmail.com>
2026-03-18 08:40:06 +0530

    Updated

--- lock.cpp
+++ lock.cpp
@@ -1,52 +1,145 @@
 #include <iostream>
+#include <filesystem>
+
+using namespace std;
+using namespace std::filesystem;
+
+void list_all(const string& dir) {
+    path p(dir);
+    if (!exists(p) || !is_directory(p)) {
+        cerr << "Invalid directory: " << dir << endl;
+        return;
+    }
+    for (auto& entry : directory_iterator(p)) {
+        cout << entry.path().filename().string() << endl;
+    }
+}
+#include <iostream>
 #include <cstdlib>
 #include <string>
 using namespace std;
 int main(int argc, char *argv[])
 {
-	if (3 > argc) {
-		cout<<"No args. type for help:\n lock help"<<endl;
-		return 1;
-	}
-	string comma = argv[1];
+  if (argc <= 1) {
+        cout<<"\033[31mError:\033[0m: lock: no arguments given \n\tUse 'lock help' for help"<<endl;
+        return 1;
+  }
 	string locker = " ~/locker/";
+  string comma = argv[1];
 	string back = " /storage/emulated/0/station/";
-	if (comma == "xml") {
+	if (comma == "-xml" || comma=="xml") {
+        if (argc < 3) {
+                cout << "\033[31mError:\033[0m Missing filename for xml command" << endl;
+                 return 1;
+        }
+    string x = argv[2];
+    string file = argv[2];
+    if (x == "-hid" || x == "hid") {
+        if (argc < 4) {
+                cout << "\033[31mError:\033[0m Missing filename for xml command" << endl;
+                 return 1;
+        }
+        file = argv[3];
+        file = "."+file;
+    }
 		string buf="mv ";
 		buf += back;
-		buf += argv[2];
+		buf += file;
 		buf += locker;
-		buf += argv[2];
-		system(buf.c_str());
-	} else if (comma=="xcl") {
+		buf += file;
+		int result = system(buf.c_str());
+    if (result != 0) {
+        cout << "\033[31mError:\033[0m:: Command failed with code " << result << endl;
+    }
+	} else if (comma=="-xcl" || comma=="xcl") {
+        if (argc < 3) {
+                cout << "\033[31mError:\033[0m Missing filename for xcl command" << endl;
+                 return 1;
+        }
+    string file = argv[2];
+    string x = argv[2];
+    if (x == "-hid" || x == "hid") {
+        if (argc < 4) {
+                cout << "\033[31mError:\033[0m Missing filename for xcl command" << endl;
+                 return 1;
+        }
+        file = argv[3];
+        file = "."+file;
+    }
 		string buf="cp ";
 		buf += back;
-		buf += argv[2];
+		buf += file;
 		buf += locker;
-		buf += argv[2];
-		system(buf.c_str());
-	} else if (comma=="xmb") {
+		buf += file;
+		int result = system(buf.c_str());
+    if (result != 0) {
+        cout << "\033[31mError:\033[0m:: Command failed with code " << result << endl;
+    }
+	} else if (comma=="-xmb" || comma=="xmb") {
+        if (argc < 3) {
+                cout << "\033[31mError:\033[0m Missing filename for xmb command" << endl;
+                 return 1;
+        }
+    string file = argv[2];
+    string x = argv[2];
+    if (x == "-hid" || x == "hid") {
+        if (argc < 4) {
+                cout << "\033[31mError:\033[0m Missing filename for xmb command" << endl;
+                 return 1;
+        }
+        file = argv[3];
+        file = "."+file;
+    }
 		string buf="mv ";
 		buf += locker;
-		buf += argv[2];
+		buf += file;
 		buf += back;
-		buf += argv[2];
-		system(buf.c_str());
-	} else if (comma=="xcb") {
+		buf += file;
+		int result = system(buf.c_str());
+    if (result != 0) {
+        cout << "\033[31mError:\033[0m:: Command failed with code " << result << endl;
+    }
+	} else if (comma=="-xcb" || comma=="xcb" ) {
+        if (argc < 3) {
+                cout << "\033[31mError:\033[0m Missing filename for xcb command" << endl;
+                 return 1;
+        }
+    string file = argv[2];
+    string x = argv[2];
+    if (x == "-hid" || x == "hid") {
+        if (argc < 4) {
+                cout << "\033[31mError:\033[0m Missing filename for xcb command" << endl;
+                 return 1;
+        }
+        file = argv[3];
+        file = "."+file;
+    }
 		string buf="cp ";
 		buf += locker;
-		buf += argv[2];
+		buf += file;
 		buf += back;
-		buf += argv[2];
-		system(buf.c_str());
-	} else if (comma=="help") {
-		string hel = "Help For genr:\n\txml = move the file to the locker.\n\txcl = copy the file to the locker file.\n\txcb = copy the file back to the home storage with '.' carrecter for hiding.\n\txmb = move the file back to the home storage.\n\thelp = to print help doc.\n\t\nAuthor : Suryalal Singha\n Contact : surya.geek.email@gmail.com\n";
+		buf += file;
+		int result = system(buf.c_str());
+    if (result != 0) {
+        cout << "\033[31mError:\033[0m:: Command failed with code " << result << endl;
+    }
+	} else if (comma=="help" || comma=="-help" || comma=="-?") {
+		string hel = "Help doc For Lock:\n\t-xml = move the file to the locker.\n\t-xcl = copy the file to the locker file.\n\t-xcb = copy the file back to the home storage.\n\t-xmb = move the file back to the home storage.\n\t-help = to print help doc.\n\n\tAuthor : Suryalal Singha\n\tContact : surya.geek.email@gmail.com\n";
 		cout << hel;
 	} else {
-		cout << "error occured. \n type 'genr help'"<<endl;
+    cout << "\033[31mError:\033[0m Lock: Not a Recognised Argument '" << comma << "'\n\tUse 'lock help'" << endl;
 	}
 	return 0;
 }
-//use these commands to make this code a real command
-//g++ test.cxx -o $PREFIX/bin/lock
-//chmod +x $PREFIX/bin/lock
+//run this command to make this a command in termux:
+//g++ lock.cxx -o $PREFIX/bin/lock && chmod +x $PREFIX/bin/lock
+/* 
+ststring x = argv[2];
+if (x == "-hid" || x == "-hid") {
+    string file = argv[3];
+    file = "."+file;
+} else {ring x = argv[2];
+if (x == "-hid" || x == "-hid") {
+    string file = argv[3];
+    file = "."+file;
+} else { */
